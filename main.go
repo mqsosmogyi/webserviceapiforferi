a2123cd2917a92fc7719c5986ca161910b786475
mqsosmogyi <104466261+mqsosmogyi@users.noreply.github.com>
2022-04-30 13:09:26 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/foolin/goview"
+
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var router *chi.Mux
+var db *sql.DB
+
+type Item struct {
+	Id   int    `json:"id"`
+	Task string `json:"task`
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query("SELECT id, task FROM user2")
+	handleErr(err)
+	defer rows.Close()
+
+	content := Item{}
+
+	for rows.Next() {
+
+		var Id int
+		var Task string
+
+		err := rows.Scan(&Id, &Task)
+		handleErr(err)
+
+		save := fmt.Sprint(Id, Task)
+		fmt.Println(save)
+
+		content.Id = Id
+		content.Task = Task
+
+		idoftask := content.Id
+		nameoftask := content.Task
+
+		strid := strconv.Itoa(idoftask)
+
+		w.Write([]byte(strid))
+		w.Write([]byte(": "))
+		w.Write([]byte(nameoftask))
+		w.Write([]byte("\n"))
+
+	}
+
+	defer rows.Close()
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+
+	var task Item
+
+	json.NewDecoder(r.Body).Decode(&task)
+
+	insert, err := db.Prepare("INSERT INTO user2(task) VALUES (?)")
+	handleErr(err)
+
+	_, er := insert.Exec(task.Task)
+	handleErr(er)
+
+	fmt.Println("Task Added!")
+
+	defer insert.Close()
+
+	name := task.Task
+	w.Write([]byte("Task Added: "))
+	w.Write([]byte(name))
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+
+	delete, err := db.Prepare("DELETE FROM user2 WHERE id=?")
+	handleErr(err)
+
+	_, er := delete.Exec(id)
+	handleErr(er)
+
+	fmt.Println("Task deleted!")
+
+	defer delete.Close()
+
+	w.Write([]byte("Task Deleted!"))
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func handleErr(err error) {
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+
+	router = chi.NewRouter()
+	router.Use(middleware.Recoverer)
+
+	dbSource := fmt.Sprintf("root:GFDdsdf1234354231@tcp(127.0.0.1:3306)/golangdb")
+
+	var err error
+	db, err = sql.Open("mysql", dbSource)
+	handleErr(err)
+}
+
+func main() {
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		err := goview.Render(w, http.StatusOK, "index", goview.M{
+			"title": "Index title!",
+			"add": func(a int, b int) int {
+				return a + b
+			},
+		})
+		if err != nil {
+			fmt.Fprintf(w, "Render index error: %v!", err)
+		}
+	})
+
+	router.Get("/page", func(w http.ResponseWriter, r *http.Request) {
+		err := goview.Render(w, http.StatusOK, "page.html", goview.M{"title": "Page file title!!"})
+		if err != nil {
+			fmt.Fprintf(w, "Render page.html error: %v!", err)
+		}
+	})
+
+	router.Delete("/test/{id}", deleteTask)
+	router.Post("/test", createTask)
+	router.Get("/test", getTasks)
+
+	fmt.Println("Listening...")
+	http.ListenAndServe(":3000", router)
+}
